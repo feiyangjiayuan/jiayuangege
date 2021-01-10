70d597f6732b8bb1361d7ff46e26d5e890ec34f1
feiyangjiayuan <969149965@qq.com>
2021-01-10 12:56:58 +0800

    wo ceshi de

---
+++ MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.administrator.gittest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //这是我测试git的代码
+        setContentView(R.layout.activity_main);
+    }
+}
